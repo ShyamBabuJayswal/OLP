b0cd20f2908fda66ee21f9f4377e37d212495555
shyambabujayswal <jaisnj541@gmail.com>
2025-06-09 21:46:54 +0530

    Add context

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,10 +1,12 @@
 "use client";
 import { useUser } from '@clerk/nextjs';
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Provider({ children }) {
   const { user } = useUser();
+
+  const {userDetail,setUserDetails}=useState();
 
   useEffect(() => {
     if (user) {
@@ -19,13 +21,19 @@ function Provider({ children }) {
         email: user?.primaryEmailAddress?.emailAddress
       });
       console.log(result.data); 
+      setUserDetails(result.data);
       
     } catch (error) {
       console.error("Failed to create user:", error);
     }
   };
 
-  return <div>{children}</div>;
+  return (
+    <UserDetailsContext.Provider value={{userDetail,setUserDetails}}>
+      <div>{children}</div>
+    </UserDetailsContext.Provider>
+  
+  )
 }
 
 export default Provider;
