6ae6cf76c183bb8a12e54bde9bdde29352222517
shyambabujayswal <jaisnj541@gmail.com>
2025-06-15 10:14:02 +0530

    Add sidebar

--- app/workspace/_components/AppSidebar.jsx
+++ app/workspace/_components/AppSidebar.jsx
@@ -1,14 +1,58 @@
+"use client"
 import { Button } from "@/@/components/ui/button";
+import { SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/@/components/ui/sidebar";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+ 
+
 } from "@/components/ui/sidebar";
+import { Book, Compass, LayoutDashboard, PencilRulerIcon, PercentCircleIcon, UserCircle2Icon, WalletCards } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+ const SideBarOptions=[
+    {
+      title:'Dashboard',
+      icon:LayoutDashboard,
+      path:"/workspace"
+    },
+     {
+      title:'My Learning',
+      icon:Book,
+      path:"/workspace/my-courses"
+    },
+     {
+      title:'Explore Courses',
+      icon:Compass,
+      path:"/workspace/explore"
+    },
+     {
+      title:'AI Tools',
+      icon:PencilRulerIcon,
+      path:"/workspace/ai-tools"
+    },
+     {
+      title:'Billing',
+      icon:WalletCards,
+      path:"/workspace/billing"
+    },
+     {
+      title:'Profile',
+      icon:UserCircle2Icon,
+      path:"/workspace/profile"
+    },
+  ]
+
+
 
 export function AppSidebar() {
+  const path=usePathname();
+ 
   return (
     <Sidebar>
      <SidebarHeader className={'p-4'}>
@@ -18,6 +62,22 @@ export function AppSidebar() {
          <SidebarGroup>
            <Button>Create New Course</Button>
         </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {SideBarOptions.map((item,index)=>(
+                  <SidebarMenuItem key={index}>
+                  <SidebarMenuButton asChild className="p-7">
+                    <Link href={item.path} className={`text-[17px] ${path.includes(item.path) && 'text-primary bg-purple-50'}`}>
+                      <item.icon className="h-7 w-7"/>
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter />
