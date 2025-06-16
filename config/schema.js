60f7e4469eca1d4e4fef373ac81e21c3e86061a0
shyambabujayswal <jaisnj541@gmail.com>
2025-06-16 21:27:32 +0530

    feat(schema): add courses table with user email reference and JSON course data

--- config/schema.js
+++ config/schema.js
@@ -1,4 +1,7 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { Description } from "@radix-ui/react-dialog";
+import { duration } from "drizzle-orm/gel-core";
+import { integer, pgTable, varchar, boolean, json } from "drizzle-orm/pg-core";
+
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,3 +9,18 @@ export const usersTable = pgTable("users", {
   email: varchar({ length: 255 }).notNull().unique(),
   subscriptionId:varchar()
 });
+
+export const coursesTable=pgTable("courses",{
+  id:integer().primaryKey().generatedAlwaysAsIdentity(),
+  cid:varchar().notNull(),
+  name:varchar(),
+  description:varchar(),
+  noOfChapters:integer().notNull(),
+  includeVideo:boolean().default(false),
+  level:varchar().notNull(),
+  category:varchar(),
+  courseJson:json(),
+  userEmail: varchar({ length: 255 }).references(() => usersTable.email)
+
+})
+
