fd8b9242200a2bdd225f61c7a1b267ecb5c9701e
shyambabujayswal <jaisnj541@gmail.com>
2025-06-22 19:06:30 +0530

    fetch course data by courseId on EditCourse mount using axios

--- app/workspace/edit-course/[courseId]/page.jsx
+++ app/workspace/edit-course/[courseId]/page.jsx
@@ -1,10 +1,26 @@
 "use client"
+import axios from 'axios';
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function EditCourse() {
     const {courseId}=useParams();
+    const [loading,setLoading]=useState(false);
+    const [course,setCourse]=useState();
     console.log(courseId);
+
+    useEffect(()=>{
+        GetCourseInfo();
+    },[])
+    const GetCourseInfo=async ()=>{
+        setLoading(true);
+    const result=await axios.get('/api/courses?courseId='+courseId
+       )
+    //    console.log(result.data);
+       setLoading(false);
+       setCourse(result.data)
+
+    }
     
 
   return (
