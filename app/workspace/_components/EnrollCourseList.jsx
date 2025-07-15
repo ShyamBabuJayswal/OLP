39a150bd0f86dbf4e2ff2b877f7223c91dd21cb4
shyambabujayswal <jaisnj541@gmail.com>
2025-07-15 21:53:12 +0530

    condionally render the component

--- app/workspace/_components/EnrollCourseList.jsx
+++ app/workspace/_components/EnrollCourseList.jsx
@@ -1,19 +1,41 @@
 "use client"
+
+import { Button } from '@/@/components/ui/button'
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import { Book, LoaderCircle, PlayCircle, Settings } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import EnrollCourseCard from './EnrollCourseCard'
 
-function EnrollCourseList() {
+function EnrollCourseList({course}) {
+  const [enrolledCourseList, setEnrolledCourseList] = useState([])
+  
 
-     useEffect(()=>{
-      GetEnrollCourse()
-     },[])
-    const GetEnrollCourse=async()=>{
-      const result=await axios.get('/api/enroll-course');
-      console.log(result.data);
+  useEffect(() => {
+    GetEnrollCourse()
+  }, [])
 
-    }
-  return (
-    <div>EnrollCourseList</div>
+  const GetEnrollCourse = async () => {
+    const result = await axios.get('/api/enroll-course')
+    console.log(result.data)
+    setEnrolledCourseList(result.data)
+  }
+
+  return enrolledCourseList?.length > 0 && (
+    <div className='mt-3'>
+      <h2 className='font-bold text-xl'>Continue Learning your courses</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-5'>
+        {enrolledCourseList?.map((course, index) => (
+        <EnrollCourseCard
+  course={course?.courses} // ✅ Correct key
+  enrollCourse={course?.enrollCourse}
+  key={index}
+    />
+
+        ))}
+      </div>
+    </div>
   )
 }
 
